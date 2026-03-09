5c897e7d6103120889262224a10b82b81674ae1f
Aleksandr Lisogor (DevOps) <slateeho@gmail.com>
2026-03-09 17:59:45 +0300

    Облачная инфраструктура. Terraform (Использование Terraform в команде (Задание 2))

---
+++ HW/Cloud_infrastructure_terraform/terraform_team/terraform-05/main.tf
@@ -0,0 +1,75 @@
+#создаем облачную сеть
+resource "yandex_vpc_network" "develop" {
+  name = "develop"
+}
+
+#создаем подсеть
+resource "yandex_vpc_subnet" "develop_a" {
+  name           = "develop-${local.subnets["vpc_dev_a"].zone}"
+  zone           = local.subnets["vpc_dev_a"].zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = [local.subnets["vpc_dev_a"].v4_cidr_blocks]
+}
+
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "develop-${local.subnets["vpc_dev_b"].zone}"
+  zone           = local.subnets["vpc_dev_b"].zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = [local.subnets["vpc_dev_b"].v4_cidr_blocks]
+}
+
+module "marketing_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "develop" 
+  network_id     = module.vpc_dev.network_id
+  subnet_zones   = values(module.vpc_dev.subnet_zone)
+  subnet_ids     = values(module.vpc_dev.subnet_ids)
+  instance_name  = "webs"
+  instance_count = 2
+  image_family   = "ubuntu-2204-lts"
+  public_ip      = true
+  preemptible    = true
+
+  labels = { 
+    owner   = "lao"
+    project = "marketing"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${trimspace(file("/home/a/.ato4ka/security/.ssh/id_ed25519.pub"))}"
+  }
+}
+
+module "analytics_vm" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = "stage"
+  network_id     = module.vpc_dev.network_id
+  subnet_zones   = [module.vpc_dev.subnet_zone["vpc_dev_a"]]
+  subnet_ids     = [module.vpc_dev.subnet_ids["vpc_dev_a"]]
+  instance_name  = "web-stage"
+  instance_count = 1
+  image_family   = "ubuntu-2204-lts"
+  public_ip      = true
+  preemptible    = true
+
+  labels = { 
+    owner   = "lao"
+    project = "analytics"
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${trimspace(file("/home/a/.ato4ka/security/.ssh/id_ed25519.pub"))}"
+  }
+}
+
+#Пример передачи cloud-config в ВМ для демонстрации №3
+data "template_file" "cloudinit" {
+  template = file("${path.module}/cloud-init.yml")
+  vars = {
+    public_key = local.public_key
+  }
+}
---
+++ HW/Cloud_infrastructure_terraform/terraform_team/terraform-05/variables.tf
@@ -0,0 +1,68 @@
+###cloud vars
+
+locals {
+  public_key  = trimspace(file("/home/a/.ato4ka/security/.ssh/id_ed25519.pub"))
+  private_key = "/home/a/.ato4ka/security/.ssh/id_ed25519"
+  cloud_init  = yamldecode(file("${path.module}/cloud-init.yml"))
+  username    = local.cloud_init.users[0].name
+  
+  s3_keys = jsondecode(file("${path.module}/s3_keys.json"))
+  
+  marketing_vm_flat = [
+    for i, v in module.marketing_vm.all : {
+      name        = v.name
+      fqdn        = module.marketing_vm.fqdn[i]
+      external_ip = module.marketing_vm.external_ip_address[i]
+    }
+  ]
+
+  analytics_vm_flat = [
+    for i, v in module.analytics_vm.all : {
+      name        = v.name
+      fqdn        = module.analytics_vm.fqdn[i]
+      external_ip = module.analytics_vm.external_ip_address[i]
+    }
+  ]
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "Yandex Cloud ID"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Yandex Folder ID"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "Default zone"
+}
+
+variable "ha" {
+  type        = bool
+  default     = false
+  description = "Enable high availability"
+}
+
+variable "s3_backend" {
+  type = map(any)
+  default = {
+    bucket                      = "lao-ush244oz"
+    key                         = "terraform.tfstate"
+    region                      = "ru-central1"
+    use_lockfile                = true
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
+
+variable "yc_sync_sa" {
+  type        = string
+  default     = "s3-sync-sa"
+  description = "Service account name for S3"
+}
